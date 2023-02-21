4da89805e2fcf158d7d6f3cc9e0f183348f55c61
赵书瑞 <691363707@qq.com>
2023-02-21 14:23:35 +0800

    vueuse练习

---
+++ src/views/vitedemo/test/vueuse/useDropZone.vue
@@ -0,0 +1,27 @@
+<template>
+  <div ref="dropZoneRef">
+    Drop files here
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+<script setup lang="ts">
+import { useDropZone,useDark,useTitle } from '@vueuse/core'
+
+const dropZoneRef = ref<HTMLDivElement>()
+
+function onDrop(files: File[] | null) {
+  // called when files are dropped on zone
+}
+
+const { isOverDropZone } = useDropZone(dropZoneRef, onDrop)
+
+const isDark = useDark()
+const title = useTitle('Set title')
+
+watch(isDark, () => {
+  title.value = isDark.value ? '🌙 Good evening!' : '☀️ Good morning!'
+})
+
+</script>
+
+
